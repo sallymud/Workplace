d6c6854555cef0e45f3b5dfe052c76f1eed29897
sallymud <Nepogodina04@mail.ru>
2024-06-13 20:12:37 +0500

    finalized the structure of the schedule

--- Workplace.Client/Program.cs
+++ Workplace.Client/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IDataService, MockData>();
-builder.Services.AddSingleton<TimeTableDataService, TimeTableMockData>();
+builder.Services.AddSingleton<IScheduleDataService, ScheduleMockData>();
 
 await builder.Build().RunAsync();
--- Workplace.Shared/RawTimeTableItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Workplace.Shared
-{
-    public class RawTimeTableItem
-    {
-        public string? Slug { get; set; }
-        public string? Name { get; set; }
-        public string[] Teachers { get; set; } = new string[3];
-    }
-}
